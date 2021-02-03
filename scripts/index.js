d3123c4115bdcc6cf88c30ded94f3b86d39aa5ab
ritonpiton <rituhanych@yandex.ru>
2021-02-03 23:49:55 +0500

    mesto v2.2

---
+++ scripts/index.js
@@ -0,0 +1,102 @@
+const container = document.querySelector('.content');
+const editPopup = document.querySelector('.popup_type_edit');
+const addPopup = document.querySelector('.popup_type_add');
+const imagePopup = document.querySelector('.popup_type_image');
+// профиль
+const curName = container.querySelector('.profile__title');
+const curJob = container.querySelector('.profile__job');
+// кнопки
+const editButton = container.querySelector('.profile__edit-btn');
+const editCloseButton = editPopup.querySelector('.popup__close-btn');
+const addButton = container.querySelector('.profile__add-btn');
+const addCloseButton = addPopup.querySelector('.popup__close-btn');
+const imageCloseButton = imagePopup.querySelector('.popup__close-btn');
+const imageOpen = imagePopup.querySelector('.popup__container_type_image');
+// Находим формы в DOM
+const formElementEdit = editPopup.querySelector('.form_type_edit');
+const formElementAdd = addPopup.querySelector('.form_type_add');
+// Находим поля форм в DOM
+const nameInput = formElementEdit.querySelector('.form__input-text_type_name');
+const jobInput = formElementEdit.querySelector('.form__input-text_type_job');
+const titleInput = formElementAdd.querySelector('.form__input-text_type_place-name');
+const linkInput = formElementAdd.querySelector('.form__input-text_type_place-link');
+// просмотр картинки
+const image = imagePopup.querySelector('.popup__image');
+const title = imagePopup.querySelector('.popup__title');
+// карточки
+const placesList = document.querySelector('.places');
+const placeTemplate = document.querySelector('.place-template').content;
+
+// инициализация карточки
+function getCard(card) {
+  const placeElement = placeTemplate.cloneNode(true);
+  const placeTitle = placeElement.querySelector('.place__title');
+  const placeLink =  placeElement.querySelector('.place__image');  
+  placeElement.querySelector('.place__delete').addEventListener('click', handleDelete); 
+  placeElement.querySelector('.place__like').addEventListener('click', handleLike);
+  placeLink.addEventListener('click', () => openPreviewPicture(card));
+  placeTitle.textContent = card.name;
+  placeLink.src = card.link;
+  placeLink.alt = card.name;
+  return placeElement;
+}
+// инициализация исходного массива
+initialCards.forEach(function (element) {
+  placesList.append(getCard(element));
+})
+// лайк карточки
+function handleLike (evt) {
+  evt.target.classList.toggle('place__like_active');
+}
+// удаление карточки
+function handleDelete (evt) {
+  evt.target.closest('.place').remove();
+}
+function openPopup (currentPopup) { // инересно было узнать про camelCase, спасибо!
+  currentPopup.classList.add('popup_opened');
+}
+function closePopup (currentPopup) {
+  currentPopup.classList.remove('popup_opened');
+}
+function openEditPopup () {
+    nameInput.value = curName.textContent;
+    jobInput.value = curJob.textContent;
+    openPopup(editPopup);
+}
+function openAddPopup () {
+  openPopup(addPopup);
+}
+// просмотр картинки
+function openPreviewPicture (card) {
+  image.src = card.link;
+  title.textContent = card.name;
+  image.alt = card.name;
+  openPopup(imagePopup);
+} 
+// отправка формы профиля
+function submitEditForm (evt) {
+  evt.preventDefault();
+  curName.textContent = nameInput.value;
+  curJob.textContent = jobInput.value;
+  closePopup(editPopup);
+}
+// добавление новой карточки
+function submitAddCard (evt) {
+  evt.preventDefault();
+  // так мы еще не задавали переменные, интересно
+  const card = {
+    name: titleInput.value,
+    link: linkInput.value
+  }
+  placesList.prepend(getCard(card));
+  closePopup(addPopup);
+  formElementAdd.reset(); // пыталась до ревью сто раз сделать через reset, а сейчас получилось (вроде), ура ^_^
+}
+
+editButton.addEventListener('click', openEditPopup);
+addButton.addEventListener('click', openAddPopup);
+editCloseButton.addEventListener('click', () => closePopup(editPopup));
+addCloseButton.addEventListener('click', () => closePopup(addPopup));
+imageCloseButton.addEventListener('click', () => closePopup(imagePopup));
+formElementEdit.addEventListener('submit', submitEditForm);
+formElementAdd.addEventListener('submit', submitAddCard);
